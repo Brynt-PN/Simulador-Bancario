c49c45e5a41b3ce23dd542442eefff132d66219b
Brynt-PN <70989902bpn@gmail.com>
2024-08-30 13:13:56 -0500

    Preliminar Simulador Bancario

---
+++ app/app.js
@@ -0,0 +1,288 @@
+// CONSTANTES ----------------------------------------
+const datos = {
+    5010: ["DEPÓSITO EN EF.", {PEN: ["5010-PEN", 0.0], USD: ["5010-USD", 0.0], EUR: ["5010-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    5011: ["DEPÓSITO MB MP", {PEN: ["5011-PEN", 0.0], USD: ["5011-USD", 0.0], EUR: ["5011-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    5012: ["DEPÓSITO MB OP", {PEN: ["5012-PEN", 0.0], USD: ["5012-USD", 0.0], EUR: ["5012-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    5013: ["DEPÓSITO OB MP", {PEN: ["5013-PEN", 0.0], USD: ["5013-USD", 0.0], EUR: ["5013-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    5014: ["DEPÓSITO OB OP", {PEN: ["5014-PEN", 0.0], USD: ["5014-USD", 0.0], EUR: ["5014-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    6010: ["CHEQUE PAGADO", {PEN: ["6010-PEN", 0.0], USD: ["6010-USD", 0.0], EUR: ["6010-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    6016: ["PAGO PARCIAL DE CHEQUE", {PEN: ["6016-PEN", 0.0], USD: ["6016-USD", 0.0], EUR: ["6016-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    6015: ["CHEQUE CERTIFICADO", {PEN: ["6015-PEN", 0.0], USD: ["6015-USD", 0.0], EUR: ["6015-EUR", 0.0]},{PEN:15,USD:5}],
+    6014: ["CHEQUE DE GERENCIA", {PEN: ["6014-PEN", 0.0], USD: ["6014-USD", 0.0], EUR: ["6014-EUR", 0.0]},{PEN:15,USD:5}],
+    6011: ["RETIRO DE EFECTIVO", {PEN: ["6011-PEN", 0.0], USD: ["6011-USD", 0.0], EUR: ["6011-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    5016: ["CUOTA TARJETA", {PEN: ["5016-PEN", 0.0], USD: ["5016-USD", 0.0], EUR: ["5016-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    5017: ["CUOTA PRÉSTAMO", {PEN: ["5017-PEN", 0.0], USD: ["5017-USD", 0.0], EUR: ["5017-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    5015: ["PAGO DE SERVICIO", {PEN: ["5015-PEN", 0.0], USD: ["5015-USD", 0.0], EUR: ["5015-EUR", 0.0]},{PEN:3.50,USD:2}],
+    6012: ["PRÉSTAMO EFECTIVO", {PEN: ["6012-PEN", 0.0], USD: ["6012-USD", 0.0], EUR: ["6012-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    6013: ["RETIRO DE TARJETA", {PEN: ["6013-PEN", 0.0], USD: ["6013-USD", 0.0], EUR: ["6013-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    7010: ["COMPRA", {PEN: ["7010-PEN", 0.0], USD: ["7010-USD", 0.0], EUR: ["7010-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    7015: ["COMPRA ESPECIAL", {PEN: ["7015-PEN", 0.0], USD: ["7015-USD", 0.0], EUR: ["7015-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    7005: ["VENTA", {PEN: ["7005-PEN", 0.0], USD: ["7005-USD", 0.0], EUR: ["7005-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    7020: ["VENTA ESPECIAL", {PEN: ["7020-PEN", 0.0], USD: ["7020-USD", 0.0], EUR: ["7020-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    8005: ["OTROS", {PEN: ["8005-PEN", 0.0], USD: ["8005-USD", 0.0], EUR: ["8005-EUR", 0.0]},{PEN:3.50,USD:2}],
+    7025: ["SOBRANTE DE CAJA", {PEN: ["7025-PEN", 0.0], USD: ["7025-USD", 0.0], EUR: ["7025-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    9000: ["INGRESO DE BOVEDA", {PEN: ["9000-PEN", 0.0], USD: ["9000-USD", 0.0], EUR: ["9000-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    7030: ["FALTANTE DE CAJA", {PEN: ["7030-PEN", 0.0], USD: ["7030-USD", 0.0], EUR: ["7030-EUR", 0.0]},{PEN:0.0,USD:0.0}],
+    9005: ["EGRESO A BOVEDA", {PEN: ["9005-PEN", 0.0], USD: ["9005-USD", 0.0], EUR: ["9005-EUR", 0.0]},{PEN:0.0,USD:0.0}]
+};
+const tiposCuenta = [
+    "CUENTA CORRIENTE",
+    "CUENTA DE AHORROS",
+    "CUENTA A PLAZO FIJO",
+    "TARJETA DE CRÉDITO",
+    "CUENTA CTS",
+    "FONDOS MUTUOS",
+    "PAGO DE CUOTA",
+    "DESEMBOLSO",
+    "CERTIFICACIÓN",
+    "EMISIÓN DE CHEQUE",
+    "LEASING",
+    "FACTORING",
+    "COLEGIO",
+    "LETRAS",
+    "CLARO",
+    "ENTEL",
+    "EDELNOR",
+    "LUZ DEL SUR",
+    "SEDAPAL",
+    "MOVISTAR",
+    "OTROS"
+];
+const plazas = [
+    "LIMA",
+    "AMAZONAS",
+    "ÁNCASH",
+    "APURÍMAC",
+    "CALLAO",
+    "AREQUIPA",
+    "AYACUCHO",
+    "CAJAMARCA",
+    "CUSCO",
+    "HUANCAVELICA",
+    "HUÁNUCO",
+    "ICA",
+    "JUNÍN",
+    "LA LIBERTAD",
+    "LAMBAYEQUE",
+    "LORETO",
+    "MADRE DE DIOS",
+    "MOQUEGUA",
+    "PASCO",
+    "SAN MARTÍN",
+    "TACNA",
+    "TUMBES",
+    "UCAYALI",
+    "OTROS",
+    "EXTERIOR"
+];
+
+// PESTAÑAS ------------------------------------------
+var operacionesPage = document.getElementById("body-opData");
+var codigosPage = document.getElementById("body-codData");
+var cajaPage = document.getElementById("body-cajaData");
+
+// BOTONES CERRAR -----------------------------------
+var xOperaciones = document.getElementById("exit-OP");
+var xCodigos = document.getElementById("exit-COD");
+var xCaja = document.getElementById("exit-CAJ");
+
+// LISTAS OPCIONES ----------------------------------
+var listCodigo = document.getElementById("cod-to-inp-br");           //1
+var listTipoCuenta = document.getElementById("tc-to-inp-br");        //3
+var listPlaza = document.getElementById("pl-oc-inp-br");             //7
+var listMoneda = document.getElementById("mo-imp-inp-br");
+
+// DATOS MOSTRADOS ----------------------------------
+var datoPEN = document.getElementById("sal-s/");
+var datoUSD = document.getElementById("sal-$");
+var datoEUR = document.getElementById("sal-E");
+var datoTipoTransac = document.getElementById("tipo-transac-p");    //2
+
+// TABLAS -------------------------------------------
+var tableResults = document.getElementById("tabla-oudtpu");
+var tableCodigos = document.getElementById("codigos-info-table");
+var tableGeneral = document.getElementById("general");
+var tableEspecific = document.getElementById("especific");
+
+
+// DATOS CAPTURADOS ---------------------------------
+var datoImporte = document.getElementById("imp-imp-inp-br");         //8 format
+var datoNumCuenta = document.getElementById("nc-to-inp-br");         //4
+var datoCuentaGira = document.getElementById("cg-oc-inp-br");        //5
+var datoNumCheque = document.getElementById("nch-oc-inp-br");        //6
+var datoComision = document.getElementById("com-imp-inp-br");        //9 format
+
+// FUNTION ------------------------------------------
+// FORMAT -------------
+function divisaFormat(value, divs){
+    value = parseFloat(value);
+    if (divs == "PEN"){
+        var format = value.toLocaleString('es-PE', { style: 'currency', currency: divs });
+    }
+    else if (divs == "USD"){
+        var format = value.toLocaleString('en-US', { style: 'currency', currency: divs });
+    }
+    else{
+        var format = value.toLocaleString('es-ES', { style: 'currency', currency: divs });
+    }
+    return format
+}
+// OPEN ---------------
+function openOperaciones(){
+    operacionesPage.style.display = "block";
+}
+function openCodigos(){
+    codigosPage.style.display = "block";
+}
+function openCaja(){
+    cajaPage.style.display = "block";
+}
+// CLOSE --------------
+function closeOperaciones(){
+    operacionesPage.style.display = "none";
+}
+function closeCodigos(){
+    codigosPage.style.display = "none";
+}
+function closeCaja(){
+    cajaPage.style.display = "none";
+}
+// SHOW ---------------
+function showPEN(value){
+    datoPEN.textContent = value;
+}
+function showUSD(value){
+    datoUSD.textContent = value;
+}
+function showEUR(value){
+    datoEUR.textContent = value;
+}
+function showTransac(value){
+    datoTipoTransac.textContent = datos[value][0];
+}
+function showComision(value){
+    datoComision.value = datos[listCodigo.value][2][value];
+}
+
+// EJECUTABLES --------------------------------------
+// PASAR DATOS A LISTAS --------------
+Object.keys(datos).forEach(codigo => {
+    const option = document.createElement('option');
+    option.value = codigo;
+    option.textContent = codigo;
+
+    listCodigo.appendChild(option);
+});
+tiposCuenta.forEach(cuenta => {
+    const option = document.createElement('option');
+    option.value = cuenta;
+    option.textContent = cuenta;
+
+    listTipoCuenta.appendChild(option);
+})
+plazas.forEach(plaza =>{
+    const option = document.createElement('option');
+    option.value = plaza;
+    option.textContent = plaza;
+
+    listPlaza.appendChild(option);
+})
+// PINTANDO TABLAS ----------------
+// Tabla General Cuadre de Caja
+Object.keys(datos).forEach(codigo => {
+    // Capturamos el tbody
+    var tablebody = tableGeneral.querySelector('tbody');
+    // Creamos fila y celdas
+    var tableRow = document.createElement('tr');
+    var celda1 = document.createElement('td');
+    var celda2 = document.createElement('td');
+    var celda3 = document.createElement('td');
+    var celda4 = document.createElement('td');
+    // Asignamos Valores
+    celda1.textContent = datos[codigo][0];
+    celda2.id = datos[codigo][1].PEN[0];
+    celda2.textContent = divisaFormat(datos[codigo][1].PEN[1],"PEN");
+    celda3.id = datos[codigo][1].USD[0];
+    celda3.textContent = divisaFormat(datos[codigo][1].USD[1],"USD");
+    celda4.id = datos[codigo][1].EUR[0];
+    celda4.textContent = divisaFormat(datos[codigo][1].EUR[1],"EUR");
+    // Agregamos al DOM
+    tableRow.appendChild(celda1);
+    tableRow.appendChild(celda2);
+    tableRow.appendChild(celda3);
+    tableRow.appendChild(celda4);
+    tablebody.appendChild(tableRow);
+});
+// Tabla Codigos y Comisiones
+Object.keys(datos).forEach(codigo =>{
+    // Capturamos el tbody
+    var tablebody = tableCodigos.querySelector('tbody');
+    // Creamos filas y celdas
+    var tableRow = document.createElement('tr');
+    var celda1 = document.createElement('td');
+    var celda2 = document.createElement('td');
+    var celda3 = document.createElement('td');
+    var celda4 = document.createElement('td');
+    // Asignando Valores
+    celda1.textContent = codigo;
+    celda2.textContent = datos[codigo][0];
+    celda3.textContent = divisaFormat(datos[codigo][2].PEN,"PEN");
+    celda4.textContent = divisaFormat(datos[codigo][2].USD,"USD")
+    // Agregamos al DOM
+    tableRow.appendChild(celda1);
+    tableRow.appendChild(celda2);
+    tableRow.appendChild(celda3);
+    tableRow.appendChild(celda4);
+    tablebody.appendChild(tableRow);
+});
+function updateTableResults(){
+    // Capturamos el tbody
+    var tablebody = tableResults.querySelector('tbody');
+    // Creamos filas y celdas
+    var tableRow = document.createElement('tr');
+    var celda1 = document.createElement('td');
+    var celda2 = document.createElement('td');
+    var celda3 = document.createElement('td');
+    var celda4 = document.createElement('td');
+    var celda5 = document.createElement('td');
+    var celda6 = document.createElement('td');
+    var celda7 = document.createElement('td');
+    var celda8 = document.createElement('td');
+    var celda9 = document.createElement('td');
+    // Asignando Valores
+    celda1.textContent = listCodigo.value;
+    celda2.textContent = datoTipoTransac.textContent;
+    celda3.textContent = listTipoCuenta.value;
+    celda4.textContent = datoNumCuenta.value;
+    celda5.textContent = datoCuentaGira.value;
+    celda6.textContent = datoNumCheque.value;
+    celda7.textContent = listPlaza.value;
+    celda8.textContent = divisaFormat(datoImporte.value, listMoneda.value);
+    celda9.textContent = divisaFormat(datoComision.value, listMoneda.value);
+    // Agregamos al DOM
+    tableRow.appendChild(celda1);
+    tableRow.appendChild(celda2);
+    tableRow.appendChild(celda3);
+    tableRow.appendChild(celda4);
+    tableRow.appendChild(celda5);
+    tableRow.appendChild(celda6);
+    tableRow.appendChild(celda7);
+    tableRow.appendChild(celda8);
+    tableRow.appendChild(celda9);
+    tablebody.appendChild(tableRow);
+}
+
+// OPERATION ----------
+function saveOP(){
+    // Identificamos el ID
+    var id = datos[listCodigo.value][1][listMoneda.value][0];
+    // Aumentamos el valor en DATOS
+    datos[listCodigo.value][1][listMoneda.value][1] += parseFloat(datoImporte.value);
+    var importe = datos[listCodigo.value][1][listMoneda.value][1];
+    // Obtenemos la celda
+    var containBox = document.getElementById(id);
+    // Actualizamos el valor en Cuadre Caja
+    containBox.textContent = divisaFormat(importe, listMoneda.value);
+    // Registramos el Movimiento
+    updateTableResults()
+}
